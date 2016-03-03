c212bba5fe85a5318484fa1e91ccf0a9fb60f39d
dfederico <dfederico7@gmail.com>
2016-03-02 20:49:55 -0600

    update head meta, but also attempting to add ng-click function for geolocation

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,18 @@
 angular.module('precip', ['ionic'])
   .controller('PrecipCtrl', function($scope, $http) {
-    $http.get('json/mplsweatherprecip.json')
+    ionic.Platform.ready(function(){
+    // will execute when device is ready, or immediately if the device is already ready.
+      console.log("now ready");
+
+      // navigator.geolocation.getCurrentPosition(disp);
+
+      // function disp(pos) {
+      //   // $scope.lat = pos.coords.latitude;
+      //   // $scope.long = pos.coords.longitude;
+      //   console.log(pos.coords.latitude);
+      // }
+
+      $http.get('json/mplsweatherprecip.json')
     // $http.get('json/mplsweather.json')
       .success(function(data) {
         // $scope.data = data.currently;
@@ -8,49 +20,60 @@ angular.module('precip', ['ionic'])
         preciplogic(rawdata);
         precipiconlogic(rawdata);
       });
-    preciplogic = function (data) {
-      if (data.precipIntensity !== 0) {
-        $scope.isPrecip = "YOU KNOW IT - " + data.precipType.toUpperCase();
+      preciplogic = function (data) {
+        if (data.precipIntensity !== 0) {
+          $scope.isPrecip = "YOU KNOW IT - " + data.precipType.toUpperCase();
+          console.log("testing one");
+        }
+        else {
+          $scope.isPrecip = "nahhhh, it's just " + data.summary.toLowerCase();
+        }
       }
-      else {
-        $scope.isPrecip = "nahhhh, it's just " + data.summary.toLowerCase();
+      $scope.clicker = function () {
+        navigator.geolocation.getCurrentPosition(disp);
+
+        function disp(pos) {
+          // $scope.lat = pos.coords.latitude;
+          // $scope.long = pos.coords.longitude;
+          console.log(pos.coords.latitude);
+        }
       }
-    }
-    precipiconlogic = function (data) {
-      if (data.icon ===  "clear-day") {
-        $scope.icon = "wi-day-sunny";
+      precipiconlogic = function (data) {
+        if (data.icon ===  "clear-day") {
+          $scope.icon = "wi-day-sunny";
+        }
+        else if (data.icon === "clear-night") {
+          $scope.icon = "wi-night-clear";
+        }
+        else if (data.icon === "rain") {
+          $scope.icon = "wi-day-rain";
+        }
+        else if (data.icon === "snow") {
+          $scope.icon = "wi-day-snow";
+        }
+        else if (data.icon === "sleet") {
+          $scope.icon = "wi-day-sleet";
+        }
+        else if (data.icon === "wind") {
+          $scope.icon = "wi-day-windy";
+        }
+        else if (data.icon === "fog") {
+          $scope.icon = "wi-day-fog";
+        }
+        else if (data.icon === "cloudy") {
+          $scope.icon = "wi-day-cloudy";
+        }
+        else if (data.icon === "partly-cloudy-day") {
+          $scope.icon = "wi-day-sunny-overcast";
+        }
+        else if (data.icon === "partly-cloudy-night") {
+          $scope.icon = "wi-night-alt-cloudy";
+        }
+        else {
+          $scope.icon = "wi-day-sunny";
+        }
       }
-      else if (data.icon === "clear-night") {
-        $scope.icon = "wi-night-clear";
-      }
-      else if (data.icon === "rain") {
-        $scope.icon = "wi-day-rain";
-      }
-      else if (data.icon === "snow") {
-        $scope.icon = "wi-day-snow";
-      }
-      else if (data.icon === "sleet") {
-        $scope.icon = "wi-day-sleet";
-      }
-      else if (data.icon === "wind") {
-        $scope.icon = "wi-day-windy";
-      }
-      else if (data.icon === "fog") {
-        $scope.icon = "wi-day-fog";
-      }
-      else if (data.icon === "cloudy") {
-        $scope.icon = "wi-day-cloudy";
-      }
-      else if (data.icon === "partly-cloudy-day") {
-        $scope.icon = "wi-day-sunny-overcast";
-      }
-      else if (data.icon === "partly-cloudy-night") {
-        $scope.icon = "wi-night-alt-cloudy";
-      }
-      else {
-        $scope.icon = "wi-day-sunny";
-      }
-    }
+    });
   })
   .directive('precipDirective', function() {
     return {
